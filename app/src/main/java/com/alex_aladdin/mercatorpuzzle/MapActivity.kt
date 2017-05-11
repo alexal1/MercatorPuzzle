977a1428482c5e99d4a30457eaf8955653172c9e
Alexander Mishchenko <5740235@gmail.com>
2017-05-11 07:58:12 +0400

    Add Mapbox map

--- app/src/main/java/com/alex_aladdin/mercatorpuzzle/MapActivity.kt
+++ app/src/main/java/com/alex_aladdin/mercatorpuzzle/MapActivity.kt
@@ -2,12 +2,64 @@ package com.alex_aladdin.mercatorpuzzle
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.mapbox.mapboxsdk.Mapbox
+import kotlinx.android.synthetic.main.activity_map.*
 
 class MapActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Get Mapbox instance
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+
         setContentView(R.layout.activity_map)
+
+        // Create a mapView
+        mapView.onCreate(savedInstanceState)
+        // Add a MapboxMap
+        mapView.getMapAsync { mapboxMap ->
+            // Configure appearance
+            mapboxMap.uiSettings.isRotateGesturesEnabled = false
+            mapboxMap.uiSettings.isCompassEnabled = false
+            mapboxMap.uiSettings.isAttributionEnabled = false
+            mapboxMap.uiSettings.isLogoEnabled = false
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    public override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
     }
 
 }
